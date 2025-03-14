45aaabf657fcaa3f7ab64ac6112a5d0a19c2a32f
taha jaiti <kyojin.rar@gmail.com>
2025-03-14 10:09:17 +0000

    FEAT: implemnted the register method in the auth controller with sanctum tokens

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Helpers\Res;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('car')->plainTextToken;
+
+        return Res::success([
+            'token'=> $token,
+            'user'=> $user
+        ], 'User registered successfully');
+    }
 }
