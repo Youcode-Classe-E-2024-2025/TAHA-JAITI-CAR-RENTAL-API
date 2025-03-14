bce5f9cea17f82534d6eabd62fda779d5f45bc5f
taha jaiti <kyojin.rar@gmail.com>
2025-03-14 10:10:51 +0000

    FEAT: implemntation of the login method with perpoer validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Helpers\Res;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -30,5 +31,28 @@ class AuthController extends Controller
             'token'=> $token,
             'user'=> $user
         ], 'User registered successfully');
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255|exists:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return Res::success([
+            'token' => $token,
+            'user' => $user
+        ], 'Logged in successfully');
     }
 }
